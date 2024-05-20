a8bbb62f7d03ef0be56742f5a8d6311bb6d7cca8
Mayank-choudhary-18 <127588707+Mayank-choudhary-18@users.noreply.github.com>
2024-05-20 21:41:26 +0530

    Initial commit

---
+++ src/ProductCard.jsx
@@ -0,0 +1,21 @@
+// components/ProductCard.js
+// import React from 'react';
+
+const ProductCard = ({ product }) => {
+  return (
+    <>
+    <div className="card">
+      {/* <img src={product.image} alt={product.title} /> */}
+      <div className="card-body">
+        <h5 className="card-title">{product.title}</h5>
+        <p className="card-text">{product.description}</p>
+        <div className="card-footer">
+          <span className="price">${product.price}</span>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default ProductCard;
